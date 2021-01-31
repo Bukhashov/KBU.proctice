3ee3db9483e8f717372adb654d29f339c659d2e2
Berik Bukhashov <bukhashov2301@mail.ru>
2021-01-31 18:27:36 +0600

    add: files first week

---
+++ 1 week/Task_5/Task_5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double r, h, S;
+            Console.WriteLine("r: ");
+            r = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("h: ");
+            h = Convert.ToDouble(Console.ReadLine());
+
+            S = (2 * Math.PI * r * h) + (2 * Math.PI * Math.Pow(r, 2));
+            
+            Console.WriteLine("S: " + Convert.ToString(S));
+        }
+    }
+}
