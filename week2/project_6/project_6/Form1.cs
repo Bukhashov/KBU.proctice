1a8552cc83f04d5e5d51ad7586bf1603e37cecd7
Berik Bukhashov <bukhashov2301@mail.ru>
2021-02-05 01:26:56 +0600

    add file week2 task 6

---
+++ week2/project_6/project_6/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace project_6
+{
+    public partial class Form1 : Form
+    {
+        private double B, X, Y, Z;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            textBox4.Text +=  "Sutdent: Bukhashov Berik";
+            textBox4.Text += Environment.NewLine + "Task: 6";
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            X = double.Parse(textBox1.Text);
+            textBox4.Text += Environment.NewLine + "X: " + X.ToString();
+            Y = double.Parse(textBox2.Text);
+            textBox4.Text += Environment.NewLine + "Y: " + Y.ToString();
+            Z = double.Parse(textBox3.Text);
+            textBox4.Text += Environment.NewLine + "Z: " + Z.ToString();
+            
+            B = Math.Sqrt(10 * (Math.Pow(Math.Sqrt(X),3) + Math.Pow(X, Y+2)))  * (Math.Pow(Math.Asin(Z),2) - Math.Abs(X - Y));
+
+            textBox4.Text += Environment.NewLine + "B: " + B.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            textBox2.Text = "";
+            textBox3.Text = "";
+            textBox4.Text = "";
+            textBox4.Text +=  "Sutdent: Bukhashov Berik";
+            textBox4.Text += Environment.NewLine + "Task: 6";
+        }
+    }
+}
