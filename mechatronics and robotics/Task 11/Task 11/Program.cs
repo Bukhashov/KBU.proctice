35803ae8cb4fe9895a5ffe9b17e1b22fd9ef4eae
Berik Bukhashov <bukhashov2301@mail.ru>
2021-02-17 01:12:47 +0600

    add files mechatronics and robotics

---
+++ mechatronics and robotics/Task 11/Task 11/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Task_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, f;
+                n = Convert.ToInt32(Console.ReadLine());
+                f = (int) Factorial(n);
+            
+                Console.WriteLine(f);
+        }
+
+        static decimal Factorial(int n1)
+        {
+            if (n1 == 0)
+            {
+                return 1;
+            }
+            else
+            {
+                return n1 * Factorial(n1 - 1);
+            }
+        }
+    }
+}
---
+++ mechatronics and robotics/Task 9/Task 9/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Task_9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("number: " + number);
+            
+        }
+    }
+}
