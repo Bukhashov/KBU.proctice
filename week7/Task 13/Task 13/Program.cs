b575d6e48a6056d06cb7e10ecf8b61bfde56638a
Berik Bukhashov <bukhashov2301@mail.ru>
2021-03-19 21:57:43 +0600

    feat: add SRSP 7week

---
+++ week7/Task 13/Task 13/Program.cs
@@ -0,0 +1,26 @@
+﻿/*  Ввести целочисленный массив, состоящий из 15 элементов.
+ *  Определить сумму и разность максимального и минимального элементов.
+ */
+
+using System;
+using System.Linq;
+
+namespace Task_13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] Array = new int[15];
+            Random RanNumber = new System.Random();
+            
+            for (int i = 0; i < 15; i++)
+            {
+                Array[i] = RanNumber.Next(20, 50);
+                Console.Write(Array[i] + " ");
+            }
+            
+            Console.WriteLine("\nMax:" + Array.Max() + "\nMin: " + Array.Min());
+        }
+    }
+}
