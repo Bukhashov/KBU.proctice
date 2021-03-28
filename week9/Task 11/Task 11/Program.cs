e16c510ced9dc294ca5761ca4cd9a624307ca0fb
Berik Bukhashov <bukhashov2301@mail.ru>
2021-03-28 19:00:16 +0600

    feat: add file week 9 SRSP

---
+++ week9/Task 11/Task 11/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int plus = 0,
+                aritEnv = 0,
+                k = 0;
+            int[,] arr = new int[3, 3] {
+                {9, 8, 7},
+                {6, 5, 4},
+                {3, 2, 1}
+            };
+            
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    plus += arr[i, j];
+                    k++;
+                }
+            }
+            aritEnv = plus / k;
+            Console.Write("Sandar qosindisi: " + plus  + "\nArifmetikaliq ortasi: " + aritEnv);
+            
+            
+        }
+    }
+}
