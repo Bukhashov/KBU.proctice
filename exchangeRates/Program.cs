897092f97038a73305c6c79436d9aa61ee48dcb0
simple <simple@simple>
2021-05-02 11:28:20 +0600

    feat created exchange rates file

---
+++ exchangeRates/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace exchangeRates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
